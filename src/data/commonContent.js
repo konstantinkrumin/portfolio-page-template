0814a21a966d8c56bbbb673e60ade4de828a2e00
Konstantin Krumin <konstantin.krumin@gmail.com>
2020-09-08 01:49:18 +0300

    added markdown previewer image

--- src/data/commonContent.js
+++ src/data/commonContent.js
@@ -1,123 +1,125 @@
 const commonContent = {
   portfolio: {
     calculator: {
-      imagePreview: "https://i.imgur.com/QnETdyT.jpg",
+      imagePreview: 'https://i.imgur.com/QnETdyT.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/yLexQmy",
-        codeSource: "https://github.com/konstantinkrumin/javascript-calculator"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/yLexQmy',
+        codeSource: 'https://github.com/konstantinkrumin/javascript-calculator',
+      },
     },
     pomodoroClock: {
-      imagePreview: "https://i.imgur.com/TQIucZr.jpg",
+      imagePreview: 'https://i.imgur.com/TQIucZr.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/ZEbZNOa",
-        codeSource: "https://github.com/konstantinkrumin/pomodoro-clock"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/ZEbZNOa',
+        codeSource: 'https://github.com/konstantinkrumin/pomodoro-clock',
+      },
     },
     drumMachine: {
-      imagePreview: "https://i.imgur.com/LVrnFeX.jpg",
+      imagePreview: 'https://i.imgur.com/LVrnFeX.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/wvMLxeQ",
-        codeSource: "https://github.com/konstantinkrumin/drum-machine"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/wvMLxeQ',
+        codeSource: 'https://github.com/konstantinkrumin/drum-machine',
+      },
     },
     markdownPreviewer: {
-      imagePreview: "https://i.imgur.com/72aHjJo.jpg",
+      imagePreview: '../images/portfolio-preview/mardown-previewer.jpg',
+      // imagePreview: 'https://i.imgur.com/72aHjJo.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/YzweoYo",
-        codeSource: "https://github.com/konstantinkrumin/markdown-previewer"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/YzweoYo',
+        codeSource: 'https://github.com/konstantinkrumin/markdown-previewer',
+      },
     },
     randomQuoteMachine: {
-      imagePreview: "https://i.imgur.com/PgVJw4j.jpg",
+      imagePreview: 'https://i.imgur.com/PgVJw4j.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/LYGQepE",
-        codeSource: "https://github.com/konstantinkrumin/random-quote-machine"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/LYGQepE',
+        codeSource: 'https://github.com/konstantinkrumin/random-quote-machine',
+      },
     },
     speedTypingGame: {
-      imagePreview: "https://i.imgur.com/XoSRVRG.jpg",
+      imagePreview: 'https://i.imgur.com/XoSRVRG.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/VwvNyqq",
-        codeSource: "https://github.com/konstantinkrumin/speed-typing-game"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/VwvNyqq',
+        codeSource: 'https://github.com/konstantinkrumin/speed-typing-game',
+      },
     },
     picSomeApp: {
-      imagePreview: "https://i.imgur.com/I4gV49G.jpg",
+      imagePreview: 'https://i.imgur.com/I4gV49G.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/jOWNodP",
-        codeSource: "https://github.com/konstantinkrumin/pic-some-app"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/jOWNodP',
+        codeSource: 'https://github.com/konstantinkrumin/pic-some-app',
+      },
     },
     heatMap: {
-      imagePreview: "https://i.imgur.com/UbPHfjF.jpg",
+      imagePreview: 'https://i.imgur.com/UbPHfjF.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/poJXjjy",
-        codeSource: "https://github.com/konstantinkrumin/dataviz-heat-map-d3"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/poJXjjy',
+        codeSource: 'https://github.com/konstantinkrumin/dataviz-heat-map-d3',
+      },
     },
     choroplethMap: {
-      imagePreview: "https://i.imgur.com/6AB6N0b.jpg",
+      imagePreview: 'https://i.imgur.com/6AB6N0b.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/JjYjpQd",
+        liveProject: 'https://codepen.io/konstantinkrumin/full/JjYjpQd',
         codeSource:
-          "https://github.com/konstantinkrumin/dataviz-choropleth-map-d3"
-      }
+          'https://github.com/konstantinkrumin/dataviz-choropleth-map-d3',
+      },
     },
     scatterplotDiagram: {
-      imagePreview: "https://i.imgur.com/uiGFb5O.jpg",
+      imagePreview: 'https://i.imgur.com/uiGFb5O.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/QWbeNpZ",
-        codeSource: "https://github.com/konstantinkrumin/dataviz-scatterplot-d3"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/QWbeNpZ',
+        codeSource:
+          'https://github.com/konstantinkrumin/dataviz-scatterplot-d3',
+      },
     },
     barChart: {
-      imagePreview: "https://i.imgur.com/QRRE3mm.jpg",
+      imagePreview: 'https://i.imgur.com/QRRE3mm.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/KKpLWdo",
-        codeSource: "https://github.com/konstantinkrumin/dataviz-bar-chart-d3"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/KKpLWdo',
+        codeSource: 'https://github.com/konstantinkrumin/dataviz-bar-chart-d3',
+      },
     },
     treemapDiagram: {
-      imagePreview: "https://i.imgur.com/ee9gqSk.jpg",
+      imagePreview: 'https://i.imgur.com/ee9gqSk.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/qBOBMeL",
+        liveProject: 'https://codepen.io/konstantinkrumin/full/qBOBMeL',
         codeSource:
-          "https://github.com/konstantinkrumin/dataviz-treemap-diagram-d3"
-      }
+          'https://github.com/konstantinkrumin/dataviz-treemap-diagram-d3',
+      },
     },
     productLandingPage: {
-      imagePreview: "https://i.imgur.com/gpp3dMp.jpg",
+      imagePreview: 'https://i.imgur.com/gpp3dMp.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/WNNNZjp",
+        liveProject: 'https://codepen.io/konstantinkrumin/full/WNNNZjp',
         codeSource:
-          "https://github.com/konstantinkrumin/static-product-landing-page"
-      }
+          'https://github.com/konstantinkrumin/static-product-landing-page',
+      },
     },
     techDocPage: {
-      imagePreview: "https://i.imgur.com/qGjITET.jpg",
+      imagePreview: 'https://i.imgur.com/qGjITET.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/eYNYbgw",
+        liveProject: 'https://codepen.io/konstantinkrumin/full/eYNYbgw',
         codeSource:
-          "https://github.com/konstantinkrumin/static-technical-doc-page"
-      }
+          'https://github.com/konstantinkrumin/static-technical-doc-page',
+      },
     },
     portfolioPageV1: {
-      imagePreview: "https://i.imgur.com/w3g4IGJ.jpg",
+      imagePreview: 'https://i.imgur.com/w3g4IGJ.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/wvaaoKY",
-        codeSource: "https://github.com/konstantinkrumin/static-portfolio-page"
-      }
+        liveProject: 'https://codepen.io/konstantinkrumin/full/wvaaoKY',
+        codeSource: 'https://github.com/konstantinkrumin/static-portfolio-page',
+      },
     },
     portfolioPageThisSite: {
-      imagePreview: "https://i.imgur.com/WfhbcKx.jpg",
+      imagePreview: 'https://i.imgur.com/WfhbcKx.jpg',
       links: {
-        liveProject: "https://codepen.io/konstantinkrumin/full/PoNbdBg",
+        liveProject: 'https://codepen.io/konstantinkrumin/full/PoNbdBg',
         codeSource:
-          "https://github.com/konstantinkrumin/portfolio-page-template"
-      }
-    }
-  }
+          'https://github.com/konstantinkrumin/portfolio-page-template',
+      },
+    },
+  },
 };
 
 export default commonContent;
